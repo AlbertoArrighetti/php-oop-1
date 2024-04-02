5cc94e4e761c98b0c2de7cf40a7c763d1c3dca41
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-02 16:24:21 +0200

    Bonus 3 completato

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 
+class Director {
+    public $name;
+    public $surname;
+    public $nationality;
+    
+    /**
+     * __construct
+     *
+     * @param  string $_name
+     * @param  string $_surname
+     * @param  string $_nationality
+     */
+    function __construct($_name, $_surname, $_nationality) {
+        $this->name = $_name;
+        $this->surname = $_surname;
+        $this->nationality = $_nationality;
+    }
+
+
+    // stringa completa del regista 
+    public function getFullDirector() {
+        return $this->name . ' ' . $this->surname . ', ' . $this->nationality;
+    }
+}
+
+
+
+
 /**
  * Class for a single movie
  */
@@ -11,6 +39,7 @@ class Movie {
     public $underage;
 
     public $genres;
+    public $director;
 
     /**
      * __construct
@@ -20,13 +49,14 @@ class Movie {
      * @param  string $_language
      * @param  array $_genres
      */
-    function __construct($_title, $_underage, $_language, array $_genres)
+    function __construct($_title, $_underage, $_language, array $_genres, Director $_director)
     {
         $this->title = $_title;
         $this->language = $_language;
         $this->underage = $_underage;
 
         $this->genres = $_genres;
+        $this->director = $_director;
     }
 
     // metodo
@@ -39,10 +69,18 @@ class Movie {
     }
 }
 
-$movie1 = new Movie('A Clockwork Orange', true, 'English', ['Horror']);
+
+
+
+
+$movieDirector1 = new Director('Stanley', 'Kubrick', 'British');
+$movie1 = new Movie('A Clockwork Orange', true, 'English', ['Horror'], $movieDirector1);
 $movie1->description = 'A Clockwork Orange, novel by Anthony Burgess, published in 1962. Set in a dismal dystopian England, it is the first-person account of a juvenile delinquent who undergoes state-sponsored psychological rehabilitation for his aberrant behaviour';
 
-$movie2 = new Movie('Nightmare', true, 'English', ['Grottesco', 'Drammatico']);
+$movieDirector2 = new Director('Wes', 'Craven', 'American');
+$movie2 = new Movie('Nightmare', true, 'English', ['Grottesco', 'Drammatico'], $movieDirector2);
+
+
 
 $movies = [
     $movie1,

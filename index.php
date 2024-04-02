e627cb9920285564ad512f2e4259b70a7d5e823b
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-02 15:20:14 +0200

    classe, construct e metodo

--- index.php
+++ index.php
@@ -1,5 +1,62 @@
 <?php
+
+/**
+ * Class for a single movie
+ */
+class Movie {
+    // attributes
+    public $title;
+    public $description;
+    public $language;
+    public $underage;
+
+
+    
+    // construct    
+    /**
+     * __construct
+     *
+     * @param  string $_title
+     * @param  string $_description
+     * @param  boolean $_underage
+     */
+    function __construct($_title, $_description, $_underage)
+        {
+            $this->title = $_title;
+            $this->description = $_description;
+            $this->underage = $_underage;
+        }
+
+
+    // metodo
+    public function isUnderAge() {
+        if ($this->underage) {
+            echo 'Questo film non è adatto ai minori di 18 anni';
+        } else {
+            echo 'Questo film è adatto ai minori di 18 anni';
+        }
+    }
+}
+
+
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,7 +75,7 @@
 
 
 
-    
+
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
